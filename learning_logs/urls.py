39389f35cb5f3ae42e1e640d30972ea17c7d35a6
JENiN2 <hlfan2@yandex.ru>
2022-07-24 18:25:32 +0700

    add index page

---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+'''Определение ссхемы URL для learning_logs.'''
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+        # Домашняя страница
+        path('', views.index, name='index')
+]
