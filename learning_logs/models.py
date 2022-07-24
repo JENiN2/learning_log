ddce91d72b0d82e4a9b65ef7333cd65d7c371055
JENiN2 <hlfan2@yandex.ru>
2022-07-24 14:08:40 +0700

    add Entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
+
 class Topic(models.Model):
-    """Тема, которую изучает пользователь"""
+    '''Тема, которую изучает пользователь'''
     lable = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         '''Возвращает строковое представление модели'''
         return self.lable
+
+
+class Entry(models.Model):
+    '''Информация, изученная пользователем по теме'''
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self) -> str:
+        '''Возвращает строковое представление модели'''
+        return self.text[:50] + '...'
