29467dc0e2dff2d751d86740c1c68080cb51d3f3
JENiN2 <hlfan2@yandex.ru>
2022-07-24 23:13:43 +0700

    add page with list of topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
         # Домашняя страница
         path('', views.index, name='index'),
         # Вывод всех тем
-        path('topics/', views.topics, name='topics')
+        path('topics/', views.topics, name='topics'),
+        # Страница с подробной информацией по отдельной теме
+        path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
 ]
