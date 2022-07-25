e69bc501d81e36801f6f67af4c7fef9a9525eaf8
JENiN2 <hlfan2@yandex.ru>
2022-07-25 23:07:01 +0700

    DEBUG off

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3e8c+l@(r@dx+9rb$@w=2ish=vgc5dg==koa%e@xp#6izh02ia'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['learn-tracker.herokuapp.com']
 
 
 # Application definition
