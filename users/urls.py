2ab2a3239deea355e167727117f4eb8fb6f98236
JENiN2 <hlfan2@yandex.ru>
2022-07-25 14:39:21 +0700

    feature logout

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     # Страница входа
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    # Страница выхода
+    path('logout/', views.logout_view, name='logout')
     ]
