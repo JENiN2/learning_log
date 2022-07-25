f38c864b73d1d0958e4505a735db4a36dc304f1c
JENiN2 <hlfan2@yandex.ru>
2022-07-25 14:22:40 +0700

    add login page

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+'''Определение схемы URL для пользователей'''
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+    # Страница входа
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+    ]
